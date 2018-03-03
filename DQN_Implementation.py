4a1f5f06a489f818f4f0f9fb067ba21343f840b6
Ubuntu <ubuntu@ip-172-31-88-119.ec2.internal>
2018-03-03 11:39:54 +0000

    Error Free Version

--- DQN_Implementation.py
+++ DQN_Implementation.py
@@ -7,6 +7,7 @@ import copy
 import argparse
 import numpy as np
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.autograd import Variable
 import random
 import math
@@ -22,8 +23,6 @@ class baseQNetwork(nn.Module):
     def __init__(self, env):
         super(baseQNetwork, self).__init__()
         self.env = env
-        self.input_size = self.env.observation_space.shape[0]
-        self.output_size = self.env.action_space.n
     
     def forward(self):
         pass
@@ -37,6 +36,8 @@ class baseQNetwork(nn.Module):
 class linearQNetwork(baseQNetwork):
     def __init__(self, env):
         super(linearQNetwork, self).__init__(env)
+        self.input_size = self.env.observation_space.shape[0]
+        self.output_size = self.env.action_space.n
         self.linear = nn.Linear(self.input_size, self.output_size)
     
     def forward(self, input_state):
@@ -44,79 +45,55 @@ class linearQNetwork(baseQNetwork):
         return output
     
 class mlpQNetwork(baseQNetwork):
-    def __init__(self, env, paramdict):
+    def __init__(self, env, hidden1=16, hidden2=16):
         super(mlpQNetwork, self).__init__(env)
-        self.hidden1 = paramdict['hidden1']
-        self.hidden2 = paramdict['hidden2']
-        self.hidden3 = paramdict['hidden3']
-        
+        self.input_size = self.env.observation_space.shape[0]
+        self.output_size = self.env.action_space.n
+        self.hidden1 = hidden1
+        self.hidden2 = hidden2
         self.linear1 = nn.Linear(self.input_size, self.hidden1)
-        if self.hidden2 is None:
-            self.linear2 = nn.Linear(self.hidden1, self.output_size)
-        else:
-            self.linear2 = nn.Linear(self.hidden1, self.hidden2)
-            if self.hidden3 is None:
-                self.linear3 = nn.Linear(self.hidden2, self.output_size)
-            else:
-                self.linear3 = nn.Linear(self.hidden2, self.hidden3)
-                self.linear4 = nn.Linear(self.hidden3, self.output_size)
-
+        self.linear2 = nn.Linear(self.hidden1, self.hidden2)
+        self.linear3 = nn.Linear(self.hidden2, self.output_size)
+    
     def forward(self, input_state):
-        hidden1 = nn.functional.relu(self.linear1(input_state))
-        if self.hidden2 is None:
-            output = nn.functional.relu(self.linear2(hidden1))
-        else:
-            hidden2 = nn.functional.relu(self.linear2(hidden1))
-            if self.hidden3 is None:
-                output = nn.functional.relu(self.linear3(hidden2))
-            else:
-                hidden3 = nn.functional.relu(self.linear3(hidden2))
-                output = nn.functional.relu(self.linear4(hidden3))
+        hidden1 = self.linear1(input_state)
+        hidden1 = F.relu(hidden1)
+        hidden2 = self.linear2(hidden1)
+        hidden2 = F.relu(hidden2)
+        output = self.linear3(hidden2)
         return output
 
 class convQNetwork(baseQNetwork):
-    def __init__(self, env, paramdict):
+    def __init__(self, env):
         super(mlpQNetwork, self).__init__(env)
+        self.output_size = self.env.action_space.n
+        self.conv1 = nn.Conv2d(4, 16, kernel_size=8, stride=4)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=4, stride=2)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.linear = nn.Linear(448, self.output_size)
 
     def forward(self, input_state):
-        raise NotImplementedError()
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
         
 class Replay_Memory():
 
     def __init__(self, memory_size=50000, burn_in=10000):
-
-        # The memory essentially stores transitions recorder from the agent
-        # taking actions in the environment.
-
-        # Burn in episodes define the number of episodes that are written into the memory from the 
-        # randomly initialized agent. Memory size is the maximum size after which old elements in 
-        # the memory are replaced. A simple (if not the most efficient) was to implement the memory 
-        # is as a list of transitions.
         self.memory_size = memory_size
         self.burn_in = burn_in
         self.memory = deque(maxlen=self.memory_size)
 
     def sample_batch(self, batch_size=32):
-        # This function returns a batch of randomly sampled transitions - i.e. state, action, reward, 
-        # next state, terminal flag tuples. You will feed this to your model to train.
         return random.sample(self.memory, batch_size)
 
     def append(self, transition):
-        # Appends transition to the memory.
         self.memory.append(transition)
 
 class DQN_Agent():
 
-    # In this class, we will implement functions to do the following. 
-    # (1) Create an instance of the Q Network class.
-    # (2) Create a function that constructs a policy from the Q values predicted by the Q Network. 
-    #       (a) Epsilon Greedy Policy.
-    #       (b) Greedy Policy. 
-    # (3) Create a function to train the Q Network, by interacting with the environment.
-    # (4) Create a function to test the Q Network's performance on the environment.
-    # (5) Create a function for Experience Replay.
-
-    def __init__(self, env_name, network, exp_replay, paramdict, render = False, use_cuda=False): 
+    def __init__(self, env_name, network, agent, exp_replay, paramdict, render = False, 
+                 use_cuda=False): 
         
         self.env_name = env_name
         self.env = gym.make(env_name)
@@ -124,45 +101,50 @@ class DQN_Agent():
         self.use_cuda = use_cuda
         self.loss_function = nn.MSELoss()
         self.exp_replay = exp_replay
+        self.agent = agent
+        self.network = network
         
         self.paramdict = paramdict
         self.num_episodes = self.paramdict['num_episodes']
         self.gamma = self.paramdict['gamma']
         
-        if network=='linear':
+        if self.network=='linear':
             self.model = linearQNetwork(self.env)
-        elif network=='mlp':
-            self.model = mlpQNetwork(self.env, self.paramdict)
+        elif self.network=='mlp':
+            self.model = mlpQNetwork(self.env)
+        elif self.agent=='doubledqn' and self.network=='conv':
+            self.model = convQNetwork(self.env)
+        elif self.agent=='duellingdqn' and self.network=='mlp':
+            raise NotImplementedError()
+        else:
+            raise NotImplementedError()
         
         self.transition = namedtuple('transition', ('state', 'action', 'next_state', 
                                                     'reward', 'is_terminal'))
         
-    def get_epsilon(self):
+    def get_epsilon(self, update_counter):
         eps_strat = self.paramdict['eps_strat']
         eps_start = self.paramdict['eps_start']
         eps_end = self.paramdict['eps_end']
         eps_decay = float(self.paramdict['eps_decay'])
         
         if eps_strat=='linear_decay':
-            eps_threshold = eps_start + (eps_end - eps_start) * max((self.update_counter/eps_decay),1)
+            eps_threshold = eps_start + (eps_end - eps_start) * min((update_counter/eps_decay),1)
             
         if eps_strat=='exp_decay':
             eps_decay= esp_decay/100
-            eps_threshold = eps_end + (eps_start - eps_end) * math.exp(-1.*self.update_counter/eps_decay)
+            eps_threshold = eps_end + (eps_start - eps_end) * math.exp(-1.*update_counter/eps_decay)
         
         if eps_strat=='log_decay':
             raise NotImplementedError()
             
-            
-
-    def epsilon_greedy_policy(self, qvalues, eps_fixed=None):
+    def epsilon_greedy_policy(self, qvalues, update_counter=None, eps_fixed=None):
         sample = random.random()
         if eps_fixed is not None:
             eps_threshold = eps_fixed
         else: 
-            eps_threshold = self.get_epsilon()
+            eps_threshold = self.get_epsilon(update_counter)
         if sample > eps_threshold:
-            print 
             action = qvalues.data.max(0)[1]
         else:
             action = torch.LongTensor([random.randrange(self.model.output_size)])
@@ -172,7 +154,7 @@ class DQN_Agent():
         action = qvalues.data.max(0)[1]
         return action[0]
     
-    def save_criteria(self, t_counter):
+    def early_stop(self, t_counter):
         if self.env_name == 'CartPole-v0':
             return min(t_counter)==200
         elif self.env_name == 'MountainCar-v0':
@@ -181,7 +163,6 @@ class DQN_Agent():
     def update_model(self, next_state, reward, qvalues, done):
         
         prediction = qvalues.max(0)[0]
-        
         next_state_var = Variable(torch.FloatTensor(next_state), volatile=True)
         if self.use_cuda and torch.cuda.is_available():
             next_state_var = next_state_var.cuda()
@@ -237,7 +218,7 @@ class DQN_Agent():
         
         log_every = self.paramdict['log_every']
         eval_every = self.paramdict['eval_every']
-        save_after = self.paramdict['save_after']
+        stop_after = self.paramdict['stop_after']
         batch_size = self.paramdict['batch_size']
         model_save = os.path.join('saved_models', self.paramdict['model_save'])
         
@@ -245,14 +226,13 @@ class DQN_Agent():
             self.model.cuda()
         
         self.optimizer = optim.Adam(self.model.parameters(), lr = self.paramdict['lrate'])
-        
         print ('*'*80)
         print ('Training.......')
         print ('*'*80)
-        t_counter = deque(maxlen=save_after)
-        self.update_counter = 1
-        
-        if self.exp_replay:
+        t_counter = deque(maxlen=stop_after)
+        update_counter = 1
+        max_average_reward = float('-Inf')
+        if self.exp_replay=='exp':
             self.burn_in_memory()
             
         for episode in range(1,self.num_episodes+1):
@@ -262,28 +242,34 @@ class DQN_Agent():
                 if self.use_cuda and torch.cuda.is_available():
                     state_var = state_var.cuda()
                 qvalues = self.model(state_var)
-                action = self.epsilon_greedy_policy(qvalues)
+                action = self.epsilon_greedy_policy(qvalues, update_counter)
                 next_state, reward, done, _ = self.env.step(action)
                 
-                if self.exp_replay:
+                if self.exp_replay=='exp':
                     self.replay.append(self.transition(cur_state, action, next_state, 
                                                        reward, done))
                     loss = self.update_model_with_replay(batch_size)
                 else:
                     loss = self.update_model(next_state, reward ,qvalues, done)
                     
-                self.update_counter += 1
+                update_counter += 1
                 cur_state = next_state
                 
-                if self.update_counter% eval_every == 0:
+                if update_counter% eval_every == 0:
                     avg_reward = self.test(num_test_episodes = 20, evaluate = True, verbose= False, 
-                                           num_updates = self.update_counter/10000)
+                                           num_updates = update_counter/eval_every)
                     t_counter.append(avg_reward)
-                    if trial and self.save_criteria(t_counter):
+                    if not trial and avg_reward >= max_average_reward:
                         print ('*'*80)
                         print ('Saving Best Model')
                         print ('*'*80)
+                        max_average_reward = avg_reward
                         self.model.save_model(model_save)
+                    if self.early_stop(t_counter):
+                        print ('*'*80)
+                        print ('EarlyStopping.... Training Complete')
+                        print ('*'*80)
+                        return
                 if done:
                     if verbose and episode % log_every == 0:
                         print('Episode %07d : Steps = %03d, Loss = %.2f' %(episode,t+1,loss))
@@ -328,8 +314,9 @@ class DQN_Agent():
         episode_reward_list = np.array(episode_reward_list)
         print ('*'*80)
         if evaluate:
-            print ('Num_Updates(*10000): %03d => Cumulative Reward: Mean= %f, Std= %f'  %(num_updates,
-                    episode_reward_list.mean(),episode_reward_list.std()))
+            print ('Num_Updates(*%d): %03d => Cumulative Reward: Mean= %f, Std= %f'  %(
+                    self.paramdict['eval_every'],num_updates,episode_reward_list.mean(),
+                    episode_reward_list.std()))
         else:
             print ('Cumulative Reward: Mean= %f, Std= %f'  %(episode_reward_list.mean(),
                                                              episode_reward_list.std()))
@@ -361,40 +348,36 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='Deep Q Network Argument Parser')
     parser.add_argument('--env', dest='env', type=str, default='CartPole-v0')
     parser.add_argument('--network', dest='network', type=str, default='mlp')
-    parser.add_argument('--replay', dest='replay', type=bool, default=False)
+    parser.add_argument('--replay', dest='replay', type=int, default=0)
     parser.add_argument('--agent', dest='agent', type=str, default='dqn')
     parser.add_argument('--train', dest='train', type=int, default=1)
     parser.add_argument('--load', dest='model_load', type=str, default=None)
-    parser.add_argument('--trial', dest='trial', type=bool, default=False)
-    parser.add_argument('--cuda', dest='use_cuda', type=bool, default=False)
-    parser.add_argument('--render', dest='render', type=bool, default=False)
+    parser.add_argument('--trial', dest='trial', type=int, default=0)
+    parser.add_argument('--cuda', dest='use_cuda', type=int, default=0)
+    parser.add_argument('--render', dest='render', type=int, default=0)
+    parser.add_argument('--run', dest='run', type=str, default='0')
     return parser.parse_args()
 
 def main(args):
-
     args = parse_arguments()
     env_name = args.env
     network = args.network
-    replay = args.replay
+    replay = 'exp' if args.replay else 'noexp'
     agent = args.agent
     use_cuda = args.use_cuda
     is_trial = args.trial
     model_load = args.model_load
-    paramdict = get_parameters(env_name, network, replay, agent)
+    run = args.run
+    paramdict = get_parameters(env_name, network, replay, agent, run)
 
     # You want to create an instance of the DQN_Agent class here, and then train / test it
-    if agent == 'dqn':
-        agent = DQN_Agent(env_name = env_name, network=network, exp_replay = replay, 
-                          paramdict = paramdict, use_cuda = use_cuda)
-    elif agent == 'doubledqn':
-        raise NotImplementedError()
-    else:
-        raise NotImplementedError()
+    dqn_agent = DQN_Agent(env_name = env_name, network=network, exp_replay = replay, 
+                          agent= agent, paramdict = paramdict, use_cuda = use_cuda)
 
     if args.train:
-        agent.train(verbose = True, trial=is_trial)
+        dqn_agent.train(verbose = True, trial=is_trial)
     elif os.path.exists(model_load):
-        agent.test(model_load)
+        dqn_agent.test(model_load)
 
 if __name__ == '__main__':
     main(sys.argv)
--- parameters.py
+++ parameters.py
@@ -1,134 +1,126 @@
 from collections import defaultdict
 
-def get_parameters(env, network, replay, agent):
+def get_parameters(env, network, replay, agent, run='0'):
     paramdict = defaultdict(lambda: None)
 
-    if env=='MountainCar-v0' and network=='linear' and replay==False and agent=='dqn':
+    if env=='MountainCar-v0' and network=='linear' and replay=='noexp' and agent=='dqn':
         paramdict['lrate'] = 0.0001
         paramdict['gamma'] = 1
         paramdict['eps_strat'] = 'linear_decay'
-        paramdict['eps_start'] = 1
+        paramdict['eps_start'] = 0.9
         paramdict['eps_end'] = 0.05
         paramdict['eps_decay'] = 1e6
         paramdict['eval_every'] = 10000
         paramdict['log_every'] = 10
-        paramdict['save_after'] = 10
-        paramdict['num_episodes'] = 5000
-        paramdict['model_save'] = '_'.join([env,network,str(replay),agent])
+        paramdict['stop_after'] = 10
+        paramdict['num_episodes'] = 50000
+        paramdict['model_save'] = '_'.join([env,network,str(replay),agent,run])
 
-    elif env=='MountainCar-v0' and network=='mlp' and replay==False and agent=='dqn':
-    	paramdict['lrate'] = 0.0001
-        paramdict['gamma'] = 1
-        paramdict['eps_strat'] = 'linear_decay'
-        paramdict['eps_start'] = 1
-        paramdict['eps_end'] = 0.05
-        paramdict['eps_decay'] = 1e6
-        paramdict['eval_every'] = 10000
-        paramdict['log_every'] = 10
-        paramdict['hidden1'] = 16
-        paramdict['hidden2'] = 16
-        paramdict['save_after'] = 10
-        paramdict['num_episodes'] = 5000
-        paramdict['model_save'] = '_'.join([env,network,str(replay),agent])
-
-    elif env=='MountainCar-v0' and network=='linear' and replay==True and agent=='dqn':
-    	paramdict['lrate'] = 0.0001
-        paramdict['gamma'] = 1
-        paramdict['eps_strat'] = 'linear_decay'
-        paramdict['eps_start'] = 1
-        paramdict['eps_end'] = 0.05
-        paramdict['eps_decay'] = 1e6
-        paramdict['eval_every'] = 10000
-        paramdict['log_every'] = 10
-       	paramdict['batch_size'] = 32
-        paramdict['save_after'] = 10
-        paramdict['num_episodes'] = 5000
-        paramdict['model_save'] = '_'.join([env,network,str(replay),agent])
-
-    elif env=='MountainCar-v0' and network=='mlp' and replay==True and agent=='dqn':
-    	paramdict['lrate'] = 0.0001
-        paramdict['gamma'] = 1
-        paramdict['eps_strat'] = 'linear_decay'
-        paramdict['eps_start'] = 1
-        paramdict['eps_end'] = 0.05
-        paramdict['eps_decay'] = 1e6
-        paramdict['eval_every'] = 10000
-        paramdict['log_every'] = 10
-        paramdict['hidden1'] = 16
-        paramdict['hidden2'] = 16
-        paramdict['batch_size'] = 32
-        paramdict['save_after'] = 10
-        paramdict['num_episodes'] = 5000
-        paramdict['model_save'] = '_'.join([env,network,str(replay),agent])
-
-    elif env=='MountainCar-v0' and network=='mlp' and replay==False and agent=='duelling':
-    	raise NotImplementedError()
-
-    elif env=='MountainCar-v0' and network=='conv' and replay==False and agent=='doubledqn':
-    	raise NotImplementedError()
-
-    if env=='CartPole-v0' and network=='linear' and replay==False and agent=='dqn':
+    elif env=='MountainCar-v0' and network=='mlp' and replay=='noexp' and agent=='dqn':
         paramdict['lrate'] = 0.0001
-        paramdict['gamma'] = 0.99
+        paramdict['gamma'] = 1
         paramdict['eps_strat'] = 'linear_decay'
-        paramdict['eps_start'] = 0.5
+        paramdict['eps_start'] = 0.9
         paramdict['eps_end'] = 0.05
         paramdict['eps_decay'] = 1e6
         paramdict['eval_every'] = 10000
-        paramdict['log_every'] = 100
-        paramdict['save_after'] = 10
-        paramdict['num_episodes'] = 15000
-        paramdict['model_save'] = '_'.join([env,network,str(replay),agent])
+        paramdict['log_every'] = 10
+        paramdict['stop_after'] = 10
+        paramdict['num_episodes'] = 50000
+        paramdict['model_save'] = '_'.join([env,network,str(replay),agent,run])
 
-    elif env=='CartPole-v0' and network=='mlp' and replay==False and agent=='dqn':
-    	paramdict['lrate'] = 0.0001
-        paramdict['gamma'] = 0.99
+    elif env=='MountainCar-v0' and network=='linear' and replay=='exp' and agent=='dqn':
+        paramdict['lrate'] = 0.0001
+        paramdict['gamma'] = 1
         paramdict['eps_strat'] = 'linear_decay'
-        paramdict['eps_start'] = 0.5
+        paramdict['eps_start'] = 0.9
         paramdict['eps_end'] = 0.05
         paramdict['eps_decay'] = 1e6
         paramdict['eval_every'] = 10000
-        paramdict['log_every'] = 100
-        paramdict['hidden1'] = 16
-        paramdict['hidden2'] = 16
-        paramdict['save_after'] = 10
-        paramdict['num_episodes'] = 20000
-        paramdict['model_save'] = '_'.join([env,network,str(replay),agent])
-
-    elif env=='CartPole-v0' and network=='linear' and replay==True and agent=='dqn':
-    	paramdict['lrate'] = 0.0001
-        paramdict['gamma'] = 0.99
-        paramdict['eps_strat'] = 'linear_decay'
-        paramdict['eps_start'] = 0.5
-        paramdict['eps_end'] = 0.05
-        paramdict['eps_decay'] = 1e6
-        paramdict['eval_every'] = 10000
-        paramdict['log_every'] = 100
-       	paramdict['batch_size'] = 32
-        paramdict['save_after'] = 10
-        paramdict['num_episodes'] = 15000
-        paramdict['model_save'] = '_'.join([env,network,str(replay),agent])
-
-    elif env=='CartPole-v0' and network=='mlp' and replay==True and agent=='dqn':
-    	paramdict['lrate'] = 0.0001
-        paramdict['gamma'] = 0.99
-        paramdict['eps_strat'] = 'linear_decay'
-        paramdict['eps_start'] = 0.5
-        paramdict['eps_end'] = 0.05
-        paramdict['eps_decay'] = 1e6
-        paramdict['eval_every'] = 10000
-        paramdict['log_every'] = 100
-        paramdict['hidden1'] = 16
-        paramdict['hidden2'] = 16
+        paramdict['log_every'] = 10
         paramdict['batch_size'] = 32
-        paramdict['save_after'] = 10
-        paramdict['num_episodes'] = 12500
-        paramdict['model_save'] = '_'.join([env,network,str(replay),agent])
+        paramdict['stop_after'] = 10
+        paramdict['num_episodes'] = 50000
+        paramdict['model_save'] = '_'.join([env,network,str(replay),agent,run])
 
-    elif env=='CartPole-v0' and network=='mlp' and replay==False and agent=='duelling':
+    elif env=='MountainCar-v0' and network=='mlp' and replay=='exp' and agent=='dqn':
+        paramdict['lrate'] = 0.0001
+        paramdict['gamma'] = 1
+        paramdict['eps_strat'] = 'linear_decay'
+        paramdict['eps_start'] = 0.9
+        paramdict['eps_end'] = 0.05
+        paramdict['eps_decay'] = 1e6
+        paramdict['eval_every'] = 10000
+        paramdict['log_every'] = 10
+        paramdict['batch_size'] = 32
+        paramdict['stop_after'] = 10
+        paramdict['num_episodes'] = 50000
+        paramdict['model_save'] = '_'.join([env,network,str(replay),agent,run])
+
+    elif env=='MountainCar-v0' and network=='mlp' and replay=='exp' and agent=='duelling':
         raise NotImplementedError()
 
-    elif env=='CartPole-v0' and network=='conv' and replay==False and agent=='doubledqn':
+    if env=='CartPole-v0' and network=='linear' and replay=='noexp' and agent=='dqn':
+        paramdict['lrate'] = 0.0001
+        paramdict['gamma'] = 0.99
+        paramdict['eps_strat'] = 'linear_decay'
+        paramdict['eps_start'] = 0.9
+        paramdict['eps_end'] = 0.05
+        paramdict['eps_decay'] = 1e6
+        paramdict['eval_every'] = 10000
+        paramdict['log_every'] = 100
+        paramdict['stop_after'] = 10
+        paramdict['num_episodes'] = 10000
+        paramdict['model_save'] = '_'.join([env,network,str(replay),agent,run])
+
+    elif env=='CartPole-v0' and network=='mlp' and replay=='noexp' and agent=='dqn':
+        paramdict['lrate'] = 0.001
+        paramdict['gamma'] = 0.99
+        paramdict['eps_strat'] = 'linear_decay'
+        paramdict['eps_start'] = 0.9
+        paramdict['eps_end'] = 0.05
+        paramdict['eps_decay'] = 1e6
+        paramdict['eval_every'] = 5000
+        paramdict['log_every'] = 10
+        paramdict['stop_after'] = 5
+        paramdict['num_episodes'] = 10000
+        paramdict['model_save'] = '_'.join([env,network,str(replay),agent,run])
+
+    elif env=='CartPole-v0' and network=='linear' and replay=='exp' and agent=='dqn':
+        paramdict['lrate'] = 0.0001
+        paramdict['gamma'] = 0.99
+        paramdict['eps_strat'] = 'linear_decay'
+        paramdict['eps_start'] = 0.9
+        paramdict['eps_end'] = 0.05
+        paramdict['eps_decay'] = 1e6
+        paramdict['eval_every'] = 10000
+        paramdict['log_every'] = 100
+        paramdict['batch_size'] = 32
+        paramdict['stop_after'] = 10
+        paramdict['num_episodes'] = 10000
+        paramdict['model_save'] = '_'.join([env,network,str(replay),agent,run])
+
+    elif env=='CartPole-v0' and network=='mlp' and replay=='exp' and agent=='dqn':
+        paramdict['lrate'] = 0.001
+        paramdict['gamma'] = 0.99
+        paramdict['eps_strat'] = 'linear_decay'
+        paramdict['eps_start'] = 0.9
+        paramdict['eps_end'] = 0.05
+        paramdict['eps_decay'] = 1e6
+        paramdict['eval_every'] = 5000
+        paramdict['log_every'] = 10
+        paramdict['batch_size'] = 32
+        paramdict['stop_after'] = 5
+        paramdict['num_episodes'] = 2000
+        paramdict['model_save'] = '_'.join([env,network,str(replay),agent,run])
+
+    elif env=='CartPole-v0' and network=='mlp' and replay=='exp' and agent=='duelling':
+        raise NotImplementedError()
+
+    elif env=='SpaceInvader-v0' and network=='conv' and replay=='exp' and agent=='doubledqn':
+        raise NotImplementedError()
+    
+    else:
         raise NotImplementedError()
 
     return paramdict

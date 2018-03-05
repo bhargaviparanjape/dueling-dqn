26be32af70bbdb51764b7177dbe96bfdb433b592
Ubuntu <ubuntu@ip-172-31-25-155.ec2.internal>
2018-03-05 23:45:40 +0000

    Seeding Done

--- DQN_Implementation.py
+++ DQN_Implementation.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
 from __future__ import print_function
+
+fixedseed = 0
+import numpy as np
+np.random.seed(fixedseed)
+import torch
+torch.manual_seed(fixedseed)
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import random
+random.seed(fixedseed)
+
 import os
 import gym
 import sys
 import copy
 import argparse
-import numpy as np
-import torch.nn as nn
-import torch.nn.functional as F
 from torch.autograd import Variable
-import random
 import math
 from itertools import count
-import torch.optim as optim
-import torch
 from collections import deque
 from collections import namedtuple
 import copy
@@ -181,12 +187,14 @@ class Prioritized_Replay_Memory():
 class DQN_Agent():
 
     def __init__(self, env_name, network, agent, exp_replay, paramdict, render = False, 
-                 use_cuda=False): 
+                 use_cuda=False, seed = 0): 
 
         # Initialize Agent Parameters
         self.env_name = env_name
         self.env = gym.make(env_name)
+        self.env.seed(seed)
         self.test_env = gym.make(env_name)
+        self.test_env.seed(seed)
         self.use_cuda = use_cuda
         self.loss_function = nn.MSELoss()
         self.exp_replay = exp_replay
@@ -688,7 +696,7 @@ def main(args):
     ## Create Agent
     paramdict = get_parameters(env_name, network, replay, agent, run)
     dqn_agent = DQN_Agent(env_name = env_name, network=network, exp_replay = replay, agent= agent, 
-                          paramdict = paramdict, use_cuda = use_cuda)
+                          paramdict = paramdict, use_cuda = use_cuda, seed=seed)
 
     if args.train:
         dqn_agent.set_logger(model_location=model_location, logfile= 'trainlogs.txt', trial=is_trial)
